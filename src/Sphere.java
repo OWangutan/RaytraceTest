909664f0fd40f55195adaacc100b954458564a2f
OWangutan <brianwang40646@gmail.com>
2023-02-22 09:45:22 -0600

    add Sphere class

---
+++ src/Sphere.java
@@ -0,0 +1,14 @@
+public class Sphere {
+    double x;
+    double y;
+    double z;
+    double radius;
+
+    public Sphere(double x,double y,double z,double radius){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.radius =  radius;
+
+    }
+}
